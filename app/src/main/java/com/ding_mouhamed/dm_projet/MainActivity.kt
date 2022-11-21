e07a489c3a2688077f3094fe25d8847a88b7d7e1
chending <polluxchen@foxmail.com>
2022-11-21 15:27:20 +0100

    create empty activity project

---
+++ app/src/main/java/com/ding_mouhamed/dm_projet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ding_mouhamed.dm_projet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
